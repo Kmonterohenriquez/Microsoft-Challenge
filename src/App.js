7b5d1d9e5b524b8bb94f048c515aea8e3af0c6cb
kmonterohenriquez <kevinwilding1997@gmail.com>
2020-11-04 19:28:19 -0500

    done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css'
 function App() {
   const [data, setData]= useState([])
   const [search, setSearch] = useState("")
+  const [statesData, setStatesData] =useState([])
   const getData = async()=> {
     await axios.get('https://bhamilton1000.github.io/SampleData/Web-Question-001/UnitedStatesWithCounties.json')
     .then(res => setData(res.data))
@@ -21,16 +22,34 @@ const containsSearch =
   ))
 ;
 
+const startsWithSearch =
+  data.filter(state => (
+    state.StateName.toLowerCase().startsWith(search.toLowerCase()) 
+  ))
+;
+
+const handleSearch=(name)=>  {
+  if(name === "start"){ 
+    setStatesData(startsWithSearch)
+  } else if (name === "contains"){
+    setStatesData(containsSearch)
+  }
+}
+
 console.log(data)
   return (
     <div className="App">
       <h1>Select State:</h1>
       <input type="text" placeholder="Enter State Name..." onChange={(e)=> setSearch(e.target.value)}/>
-      {containsSearch.map(curr=>(
+      
+      Start with: <input type="radio" name="search" onClick={()=> handleSearch("start")}/>
+      Contains <input type="radio" name="search" onClick={()=> handleSearch("contains")} checked/>
+      {statesData.map(curr=>(
         <div className="state">
           {curr.StateName}
         </div>
       ))}
+      
     </div>
   );
 }
